c46e59884f1af9410c720336c25da5a122426db9
hansol <hs_1022@naver.com>
2019-08-27 16:00:44 +0900

    User - ApprovalController 생성

---
+++ BitGroupware/src/main/java/com/bitgroupware/approval/controller/ApprovalController.java
@@ -0,0 +1,43 @@
+package com.bitgroupware.approval.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApprovalController {
+	
+	// 결재 받을 문서 리스트
+	public String ApprovalListTobe() {
+		return null;
+		
+	}
+	
+	// 결재 할 문서 리스트
+	public String ApprovalListTo() {
+		return null;
+	}
+	
+	// 기안하기
+	public String insertApprovalDocView() {
+		return null;
+	}
+	
+	// 저장
+	public String insertApprovalDoc() {
+		return null;
+	}
+	
+	// 읽기
+	public String approvalView() {
+		return null;
+	}
+	
+	// 삭제
+	public String deleteApproval() {
+		return null;
+	}
+	
+	// 결재
+	public String updateApproval() {
+		return null;
+	}
+}
